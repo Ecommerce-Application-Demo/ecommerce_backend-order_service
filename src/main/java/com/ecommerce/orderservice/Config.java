b7b7fcacacd02e9755e97433bbb1fe62e6b95caf
Sourav Mazumder <122155642+souravM2000@users.noreply.github.com>
2024-05-27 19:43:36 +0530

    Initial commit

---
+++ src/main/java/com/ecommerce/orderservice/Config.java
@@ -0,0 +1,43 @@
+package com.ecommerce.orderservice;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class Config {
+
+    @Autowired
+    Environment env;
+
+    @Bean
+    ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+
+    @Bean
+    OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Order Service")
+                        .description("JAVA Backend API Documentation for Ecommerce Order Service")
+                        .version("v0.1")
+                        .license(new License().name("MIT License").url("https://opensource.org/license/mit")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Github")
+                        .url("https://github.com/Ecommerce-Application-Demo/ecommerce-backend"));
+    }
+
+//    @Scheduled(fixedDelay = 1000*60*5)
+//    void renderRunner() {
+//        if (Arrays.asList(env.getActiveProfiles()).contains("prod")) {
+//            RestTemplate restTemplate = new RestTemplate();
+//            restTemplate.getForEntity("https://ecommerce-backend-product-service.onrender.com/product/actuator/info", String.class);
+//        }
+//    }
+}
