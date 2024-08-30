0a876e76fcae2ed28505865e82ebc077971d0825
Sourav Mazumder <122155642+souravM2000@users.noreply.github.com>
2024-08-30 19:24:01 +0530

    Implemented add,remove & get wishlist APIs

--- src/main/java/com/ecommerce/orderservice/Config.java
+++ src/main/java/com/ecommerce/orderservice/Config.java
@@ -8,10 +8,12 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -24,12 +26,23 @@ import java.util.Arrays;
 )
 public class Config {
 
+    @Value("${product.base.url}")
+    private String productBaseUrl;
+
     @Autowired
     Environment env;
 
     @Bean
     ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+
+    @Bean
+    RestClient productRestClient() {
+        return RestClient.builder()
+                .baseUrl(productBaseUrl)
+                .defaultHeader(Constants.PRODUCT_API_KEY, Constants.PRODUCT_API_SECRET)
+                .build();
     }
 
     @Bean
---
+++ src/main/java/com/ecommerce/orderservice/Constants.java
@@ -0,0 +1,11 @@
+package com.ecommerce.orderservice;
+
+public class Constants {
+
+    public static final String JWT_BEARER_TEXT= "Bearer ";
+    public static final String JWT_HEADER_NAME= "Authorization";
+
+    public static final String PRODUCT_GET_STYLES = "/api/order/styles";
+    public static final String PRODUCT_API_KEY = "x-functions-key";
+    public static final String PRODUCT_API_SECRET = "1213";
+}
